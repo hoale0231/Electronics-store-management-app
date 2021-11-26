6f4926eb52d71a8523f658e6ad2e2f20c83adf01
Black-Frost <vovantiendung2001@gmail.com>
2021-11-27 00:57:19 +0700

    Implement remove and update funtionality

--- server/App/Sales.py
+++ server/App/Sales.py
@@ -42,8 +42,8 @@ def getProductsOfSales():
     query = '''select SanPham.ID, ProdName, Price, manufacture
                 from CTKM_SanPham, SanPham, SanPham_ApDung_CTKM
                 where CTKM_SanPham.ID = SanPham_ApDung_CTKM.ID_Ad and
-	            SanPham.ID = SanPham_ApDung_CTKM.ID_Prod and
-	            CTKM_SanPham.ID = ?'''
+                SanPham.ID = SanPham_ApDung_CTKM.ID_Prod and
+                CTKM_SanPham.ID = ?'''
     cursor.execute(query, id)
     records = cursor.fetchall()
     colNames = [column[0] for column in cursor.description]
@@ -102,3 +102,84 @@ def applySalesToBrand():
         print("ERROR: " + str(e))
         return Response(e.args[1], status=400)
     return Response("Success", status=200)
+
+@Sales.route("/update/sales", methods=["POST"])
+def updateSales():
+    id = request.form.get("id")
+    startDate = request.form.get("startDate")
+    endDate = request.form.get("endDate")
+    rate = request.form.get("rate")
+
+    if (id == None or startDate == None or endDate == None or rate == None):
+        return Response("Not enough information", status=400)
+    try:
+        query = '''update CTKM_SanPham
+                set TimeStart = ?, TimeEnd = ?, PromoLevel = ?
+                where ID = ?'''
+        cursor.execute(query, startDate, endDate, rate, id)
+        conn.commit()
+    except pyodbc.ProgrammingError as e:
+        return Response(e.args[1], status=400)  #send sql error msg back to client
+    return Response("Success", status=200)
+
+@Sales.route("/remove/sales", methods=["POST"])
+def removeSales():
+    id = request.form.get("id")
+
+    if (id == None):
+        return Response("Please provide and ID", status=400)
+    try:
+        query = '''delete from CTKM_SanPham where ID = ?'''
+        cursor.execute(query, id)
+        conn.commit()
+    except pyodbc.ProgrammingError as e:
+        return Response(e.args[1], status=400)  #send sql error msg back to client
+    return Response("Success", status=200)
+
+@Sales.route("/remove/applied-product", methods=["POST"])
+def removeAppliedProduct():
+    salesId = request.form.get("salesId")
+    productId = request.form.get("productId")
+
+    if (id == None):
+        return Response("Please provide and ID", status=400)
+    try:
+        query = '''delete from SanPham_ApDung_CTKM where ID_Ad = ? and ID_Prod = ?'''
+        cursor.execute(query, salesId, productId)
+        conn.commit()
+    except pyodbc.ProgrammingError as e:
+        return Response(e.args[1], status=400)  #send sql error msg back to client
+    return Response("Success", status=200)
+
+@Sales.route("/get/product-sales", methods=["POST"])
+def getSalesOfProduct():
+    id = request.form.get("id")
+    startDate = request.form.get("startDate")
+    endDate = request.form.get("endDate")
+
+    if (id == None or startDate == None or endDate == None):
+        return Response("Not enough information", status=400)
+    query = '''exec getSalesByProduct ?, ?, ?'''
+    cursor.execute(query, id, startDate, endDate)
+
+    records = cursor.fetchall()
+    colNames = [column[0] for column in cursor.description]
+    results = [dict(zip(colNames, row)) for row in records]
+    return jsonify(results)
+
+@Sales.route("/get/best-sales", methods=["POST"])
+def getBestSaleOfBrand():
+    brandName = request.form.get("brandName")
+    startDate = request.form.get("startDate")
+    endDate = request.form.get("endDate")
+
+    if (brandName == None or startDate == None or endDate == None):
+        return Response("Not enough information", status=400)
+    query = '''exec getTopDealsOfBrand ?, ?, ?'''
+    cursor.execute(query, brandName, startDate, endDate)
+
+    records = cursor.fetchall()
+    colNames = [column[0] for column in cursor.description]
+    results = [dict(zip(colNames, row)) for row in records]
+    return jsonify(results)
+
