14aedd7742c687134102394248faeb987b12b291
Black-Frost <vovantiendung2001@gmail.com>
2021-11-29 23:59:15 +0700

    Fix error in insert sales data

--- app/src/Sales/SalesManagement.js
+++ app/src/Sales/SalesManagement.js
@@ -19,6 +19,7 @@ export default function SalesManagement() {
   const [products, setProducts] = useState([]);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  var [addProduct, setAddProduct] = useState(0);
   
   const columns = [
     { dataField: "ID",            text: "Sales ID",     sort: true },
@@ -123,11 +124,21 @@ export default function SalesManagement() {
         <BootstrapTable keyField="id" data={products} columns={columns} rowEvents={rowEvents}/>
       </div>
       <div className="container">
-        <Button variant="success" onClick={() => {loadSalesAll()}}>Load all</Button>
+      <Row className="g-2">
+      	<Col>
+        	<Button variant="success" onClick={() => {loadSalesAll()}}>Load all</Button>
+        </Col>
+        <Col>
+        	<Button variant="primary" onClick={() => {setAddProduct(1);}}>Add sales event</Button>
+        </Col>
+       </Row>
       </div>
       <div>
         {productDescription.id === -1 ? <p/> : <ProductDescription data={productDescription} 
           setSalesDescription={setSalesDescription} deleteSales={deleteSales} title={'Edit sales event'} action = 'edit' reload={loadSalesAll}/>}
+      </div>
+      <div>
+        {addProduct === 0 ? <p/> : <NewProduct setAddProduct={setAddProduct} title={'Add sales event'} action = 'add' reload={loadSalesAll}/>}
       </div>
     </div>
   );
@@ -140,7 +151,7 @@ function ProductDescription(props) {
   var editStartDate= new Date(data.TimeStart);
   var editEndDate = new Date(data.TimeEnd);
   var discountRate = data.PromoLevel;
-  var applyType = "";
+  var applyType = "Product";
   var applyInfo = "";  
   
   const handleSubmit = (event) => {
@@ -168,13 +179,21 @@ function ProductDescription(props) {
           	var applyQuery = applyType === "Product" ? "/api/sales/apply/product?productId=" : "/api/sales/apply/brand?brandName=";
           	fetch(applyQuery + applyInfo + "&salesId=" + ID)
       		.then(responseApply => {
-        	if (!response.ok) {
-          		responseApply.text().then(text => { alert(text);})
+        	if (responseApply.ok === false) {
+          		responseApply.text().then(text => { alert(text); reload(); setSalesDescription({id: -1});})
         	   }
+        	else{
+        	        reload();
+          		setSalesDescription({id: -1});
+        	}
       		}) 
           }
-          reload();
-          setSalesDescription({id: -1});
+          else
+          {
+          	reload();
+          	setSalesDescription({id: -1});
+          }
+
         } else {
           response.text().then(text => { alert(text) })
           event.stopPropagation();
@@ -232,6 +251,142 @@ function ProductDescription(props) {
 
         <Modal.Footer>
           <Button variant="danger" onClick={() => {deleteSales(data.ID); setSalesDescription({id: -1})}}>Delete</Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  )
+}
+
+
+function NewProduct(props) {
+  const {setAddProduct, title, action, reload} = props;
+  var ID = '';
+  var editStartDate= null;
+  var editEndDate = null;
+  var discountRate = "0";
+  var applyType = "Product";
+  var applyInfo = "";  
+  
+  const handleSubmit = (event) => {
+     if (ID ===  '')
+     {
+     	alert("Please enter an ID");       
+     	event.stopPropagation();
+     	event.preventDefault(); 
+     	return;
+     }
+     
+     if (ID.startsWith('KMSP') == false || ID.length !== 9)
+     {
+     	alert("ID must start with KMSP and has the length of 9");       
+     	event.stopPropagation();
+     	event.preventDefault(); 
+     	return;
+     }
+     
+     if (editStartDate == null || editEndDate == null)
+     {
+     	alert("Please enter start and end date");       
+     	event.stopPropagation();
+     	event.preventDefault(); 
+     	return;
+     }
+  
+     if (editStartDate > editEndDate) {
+     alert("Start date must come before end date");       
+     event.stopPropagation();
+     event.preventDefault(); 
+     return;
+     }
+     
+     if (discountRate <= 0 || discountRate >= 100) {
+     alert("Discount rate must be between 0 and 100"); 
+     event.stopPropagation();
+     event.preventDefault();
+     return;
+     }
+  
+      var query = "/api/sales/add/sales";
+      fetch(query + "?id=" + ID + "&startDate=" + editStartDate.yyyymmdd() + "&endDate=" + editEndDate.yyyymmdd() + "&rate=" + discountRate)
+      .then(response => {
+        if (response.ok) {
+
+          if (applyInfo !== "")
+          {
+          	var applyQuery = applyType === "Product" ? "/api/sales/apply/product?productId=" : "/api/sales/apply/brand?brandName=";
+          	fetch(applyQuery + applyInfo + "&salesId=" + ID)
+      		.then(responseApply => {
+        	if (responseApply.ok === false) {
+          		responseApply.text().then(text => { alert(text); reload(); setAddProduct(0);})
+        	   }
+        	else
+        	{                 
+        		reload();
+          		setAddProduct(0);
+        	}
+      		}) 
+          }
+          else{
+          reload();
+          setAddProduct(0);
+          }
+        } else {
+          response.text().then(text => { alert(text) })
+          event.stopPropagation();
+        }
+      }) 
+    if (action === "add") {
+      event.preventDefault();
+    }
+  };
+  
+  return(
+    <div className="popup-background">
+      <Modal.Dialog className="popup">
+        <Modal.Header closeButton onClick={() => setAddProduct(0)}>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formSalesID">
+              <Form.Label>Product ID</Form.Label>
+              <Form.Control type="ID" type="text" placeholder="Enter sales ID" onChange = {(event) =>{ID = event.target.value;}}/>
+            </Form.Group>
+
+            <Row className="mb-3">
+		<Form.Group as={Col} controlId="formStartDate">
+      			<Form.Label>Start date</Form.Label>
+      			<Form.Control type="date" placeholder="Enter start date" onChange={(event) => {editStartDate = new Date(event.target.value)}}/>
+    		</Form.Group>
+
+    		<Form.Group as={Col} controlId="formEndDate">
+      			<Form.Label>End date</Form.Label>
+      			<Form.Control type="date" placeholder="Enter end date" onChange={(event) => {editEndDate = new Date(event.target.value);}}/>
+    		</Form.Group>
+  	    </Row>
+  	      <Form.Group className="mb-3" controlId="formDiscount">
+    		<Form.Label>Discount rate</Form.Label>
+    		<Form.Control type="text" placeholder="Enter discount rate" onChange={(event) => {discountRate = event.target.value;}}/>
+  	      </Form.Group>
+  	      
+  	      <Form.Group className="mb-3" controlId="formApplyType">
+  	      <Form.Label>Apply for</Form.Label>
+            <Form.Select aria-label="Floating label select example" onChange={(event) => {applyType = event.target.value;}}>
+                <option value="Product">Product</option>
+                <option value="Brand">Brand</option>
+            </Form.Select>
+            
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formProdId">
+    		<Form.Label>Brand name/Product ID</Form.Label>
+    		<Form.Control type="text" placeholder="Enter Brand name/Product ID" onChange={(event) => {applyInfo = event.target.value;}}/>
+  	      </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
           <Button variant="primary" type="submit" onClick={handleSubmit}>Save changes</Button>
         </Modal.Footer>
       </Modal.Dialog>
