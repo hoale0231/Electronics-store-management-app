a8a5d0ead1d0b82bab07b907447f26e68b8f47bd
Black-Frost <vovantiendung2001@gmail.com>
2021-11-29 11:43:46 +0700

    Add edit sales functionality

--- app/src/Sales/SalesManagement.js
+++ app/src/Sales/SalesManagement.js
@@ -123,11 +123,11 @@ export default function SalesManagement() {
         <BootstrapTable keyField="id" data={products} columns={columns} rowEvents={rowEvents}/>
       </div>
       <div className="container">
-        <Button variant="success" onClick={() => {loadSalesAll()}}>Remove all filter</Button>
+        <Button variant="success" onClick={() => {loadSalesAll()}}>Load all</Button>
       </div>
       <div>
         {productDescription.id === -1 ? <p/> : <ProductDescription data={productDescription} 
-          setSalesDescription={setSalesDescription} deleteSales={deleteSales} action={'Edit Product'}/>}
+          setSalesDescription={setSalesDescription} deleteSales={deleteSales} title={'Edit sales event'} action = 'edit' reload={loadSalesAll}/>}
       </div>
     </div>
   );
@@ -135,39 +135,82 @@ export default function SalesManagement() {
 
 
 function ProductDescription(props) {
-  const {data, setSalesDescription, deleteSales, action} = props
+  const {data, setSalesDescription, deleteSales, title, action, reload} = props;
+  var ID = data.ID;
+  var editStartDate= new Date(data.TimeStart);
+  var editEndDate = new Date(data.TimeEnd);
+  var discountRate = data.PromoLevel;
+  var applyType = "";
+  var applyInfo = "";  
+  
+  const handleSubmit = (event) => {
+     if (editStartDate > editEndDate) {alert("Start date must come before end date"); return;};
+     if (discountRate <= 0 || discountRate >= 100) {alert("Discount rate must be between 0 and 100"); return;};
+  
+      var query = "/api/sales/update/sales";
+      fetch(query + "?id=" + ID + "&startDate=" + editStartDate.yyyymmdd() + "&endDate=" + editEndDate.yyyymmdd() + "&rate=" + discountRate)
+      .then(response => {
+        if (response.ok) {
+          reload();
+          setSalesDescription({id: -1});
+        } else {
+          response.text().then(text => { alert(text) })
+          event.stopPropagation();
+        }
+      }) 
+    if (action === "edit") {
+      event.preventDefault();
+    }
+  };
+  
   return(
     <div className="popup-background">
       <Modal.Dialog className="popup">
         <Modal.Header closeButton onClick={() => setSalesDescription({id:-1})}>
-          <Modal.Title>{action}</Modal.Title>
+          <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formSalesID">
               <Form.Label>Product ID</Form.Label>
-              <Form.Control type="ID" placeholder="Enter Product ID" />
+              {action === 'edit'? <Form.Control type="ID" plaintext readOnly disabled defaultValue={data.ID}/> : <Form.Control type="ID" plaintext defaultValue="Enter sales ID"/>}
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
+            <Row className="mb-3">
+		<Form.Group as={Col} controlId="formStartDate">
+      			<Form.Label>Start date</Form.Label>
+      			<Form.Control type="date" defaultValue={data.TimeStart} placeholder="Enter start date" onChange={(event) => {editStartDate = new Date(event.target.value)}}/>
+    		</Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+    		<Form.Group as={Col} controlId="formEndDate">
+      			<Form.Label>End date</Form.Label>
+      			<Form.Control type="date" defaultValue={data.TimeEnd} placeholder="Enter end date" onChange={(event) => {editEndDate = new Date(event.target.value);}}/>
+    		</Form.Group>
+  	    </Row>
+  	      <Form.Group className="mb-3" controlId="formDiscount">
+    		<Form.Label>Discount rate</Form.Label>
+    		<Form.Control type="text" defaultValue={data.PromoLevel} placeholder="Enter discount rate" onChange={(event) => {discountRate = event.target.value;}}/>
+  	      </Form.Group>
+  	      
+  	      <Form.Group className="mb-3" controlId="formApplyType">
+  	      <Form.Label>Apply for</Form.Label>
+            <Form.Select aria-label="Floating label select example" onChange={(event) => {applyType = event.target.value;}}>
+                <option value="Product">Product</option>
+                <option value="Brand">Brand</option>
+            </Form.Select>
             
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formProdId">
+    		<Form.Label>Brand name/Product ID</Form.Label>
+    		<Form.Control type="text" placeholder="Enter Brand name/Product ID" onChange={(event) => {applyInfo = event.target.value;}}/>
+  	      </Form.Group>
           </Form>
         </Modal.Body>
 
         <Modal.Footer>
           <Button variant="danger" onClick={() => {deleteSales(data.ID); setSalesDescription({id: -1})}}>Delete</Button>
-          <Button variant="primary">Save changes</Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Save changes</Button>
         </Modal.Footer>
       </Modal.Dialog>
     </div>
