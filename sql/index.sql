f1b541fef7bc4362a80cc5c1aa8de678f7b2adb9
Black-Frost <60770785+Black-Frost@users.noreply.github.com>
2021-11-30 21:33:27 +0700

    Create index.sql

---
+++ sql/index.sql
@@ -0,0 +1,4 @@
+create index brandIdx on SanPham(manufacture)
+create index prodTypeIdx on SanPham(ProdType)
+create index tbdtIdx on ThietBiDienTu(DeviceType)
+create index pkIdx on PhuKien(AccsoryType)
