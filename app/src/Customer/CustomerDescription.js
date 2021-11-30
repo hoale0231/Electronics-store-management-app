89acd2a13b62f663f59f9cb402731655c04c1a82
Tuan Trinh <caovuhoangnam@outlook.com.vn>
2021-11-30 21:41:10 +0700

    add table (but fail)

--- app/src/Customer/CustomerDescription.js
+++ app/src/Customer/CustomerDescription.js
@@ -2,11 +2,44 @@ import { Modal, Button, Form, Row, Col } from 'react-bootstrap'
 import BootstrapTable from "react-bootstrap-table-next";
 import { useState, useEffect } from "react";
 import "./CustomerDescription.css"
+import { Component } from "react";
 
-export default function CustomerDescription(props) {
-    const { id, setCustomerDescription, action } = props
-    const [info, setInfo] = useState({})
-    const columns = [
+export default class CustomerDescription extends Component {
+	constructor(props) {
+		super(props);
+		this.setCustomerDescription = props.setCustomerDescription;
+		this.state = {
+			id: props.id,
+			action: props.action,
+			info: {},
+			validated: false,
+		}
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+		if (this.state.action !== "Edit") {
+			return;
+		}
+		fetch("/get/customer?id=" + this.state.id)
+		.then((response) => {
+			if (response.ok) {
+			return response.json()
+			}
+			throw response
+		})
+		.then((data) => { 
+			console.log(data); 
+			this.setState({
+				info: data,
+			})
+		})
+		.catch((error) => {
+			console.error("Error fetching data: ", error);
+		})
+	}
+
+    columns = [
         { dataField: "ID", text: "Customer ID"},
         { dataField: "Username", text: "Username"},
         { dataField: "Phone", text: "Phone number"},
@@ -18,61 +51,47 @@ export default function CustomerDescription(props) {
         { dataField: "FamScore", text: "Familiarity point"},
     ]
 
-    useEffect(() => {
-        if (action !== "Edit") {
-          return;
-        }
-        fetch("/get/customer?id=" + id)
-          .then((response) => {
-            if (response.ok) {
-              return response.json()
-            }
-            throw response
-          })
-          .then((data) => { console.log(data); setInfo(data) })
-          .catch((error) => {
-            console.error("Error fetching data: ", error);
-          })
-    }, [id, action])
-
-    const handleInputChange = (event) => {
+    handleInputChange = (event) => {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-        info[name] = value;
+		this.state.info[name] = value;
+		this.setState({
+			info: this.state.info
+		})
     }
 
-    function isInt(value) {
+    isInt(value) {
         return !isNaN(value) && 
                parseInt(Number(value)) == value && 
                !isNaN(parseInt(value, 10));
     }
-
-    const [validated, setValidated] = useState(false);
     
-    const handleSubmit = (event) => {
+    handleSubmit = (event) => {
         const form = event.currentTarget;
-        if (!isInt(form.FamScore.value)) {
+        if (!this.isInt(form.FamScore.value)) {
           alert("Familiarity point much be number!")
           event.stopPropagation();
           event.preventDefault();
           return
         }
-        setValidated(true);
+		this.setState({
+			validated: true,
+		})
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         } else {
-          var query = action === "Edit" ? "/edit/customer" : "/add/customer"
+          var query = this.state.action === "Edit" ? "/edit/customer" : "/add/customer"
           const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(info)
+            body: JSON.stringify(this.state.info)
           };
           fetch(query, requestOptions)
             .then(response => {
               if (response.ok) {
-                setCustomerDescription(-1)
+                this.setCustomerDescription(-1)
               } else {
                 response.text().then(text => { alert(text); })
               }
@@ -80,41 +99,42 @@ export default function CustomerDescription(props) {
         }
     };
 
+	render() {
     return (
         <div className="popup-background">
         <Modal.Dialog className="popup modal-dialog-scrollable" size="lg">
-            <Modal.Header closeButton onClick={() => setCustomerDescription(-1)}>
-                <Modal.Title>{action}</Modal.Title>
+            <Modal.Header closeButton onClick={() => this.setCustomerDescription(-1)}>
+                <Modal.Title>{this.state.action}</Modal.Title>
             </Modal.Header>
             <Modal.Body className="body-popup">
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form noValidate validated={this.validated} onSubmit={this.handleSubmit}>
                 <Row className="mb-3">
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="ID" attrName="Customer ID" required={true} md="4" disable={action === 'Edit'} />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Username" attrName="Username" required={true} md="4" disable={action === 'Edit'} />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Passwd" attrName="Password" required={true} md="4" />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="ID" attrName="Customer ID" required={true} md="4" disable={this.state.action === 'Edit'} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Username" attrName="Username" required={true} md="4" disable={this.state.action === 'Edit'} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Passwd" attrName="Password" required={true} md="4" />
                 </Row>
                 <Row className="mb-3">
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Phone" attrName="Phone number" required={true} md="4" />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Fname" attrName="First name" md="4" required={true} />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Lname" attrName="Last name" md="4" required={true} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Phone" attrName="Phone number" required={true} md="4" />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Fname" attrName="First name" md="4" required={true} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Lname" attrName="Last name" md="4" required={true} />
                 </Row>
                 <Row className="mb-3">
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Email" attrName="Email" md="4" disable={action === 'Edit'} />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Bdate" attrName="Birthdate" md="4" required={true} />
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="IdNum" attrName="ID number" required={true} md="4" />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Email" attrName="Email" md="4" disable={this.state.action === 'Edit'} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Bdate" attrName="Birthdate" md="4" required={true} />
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="IdNum" attrName="ID number" required={true} md="4" />
                 </Row>
                 <Row className="mb-3">
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="FamScore" attrName="Familiarity point" md="6" required={true} />
-                    {action === 'Edit' ? 
-                    <InputGroupCustom info={info} handleInputChange={handleInputChange} attr="Num_ref" attrName="Number of Recommendees" md="6" required={true} disable={true}/> :
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="FamScore" attrName="Familiarity point" md="6" required={true} />
+                    {this.state.action === 'Edit' ? 
+                    <InputGroupCustom info={this.state.info} handleInputChange={this.handleInputChange} attr="Num_ref" attrName="Number of Recommendees" md="6" required={true} disable={true}/> :
                     <p/>}
                 </Row>
-                <Form.Group as={Col} md="12">
+                {/* <Form.Group as={Col} md="12">
                     <Form.Label>List of recommendees:</Form.Label>
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                            {columns.map((item, i) => (
+                            {this.columns.map((item, i) => (
                                 <th name={item.dataField}>
                                     {item.text}
                                 </th>
@@ -122,10 +142,20 @@ export default function CustomerDescription(props) {
                             </tr>
                         </thead>
                         <tbody>
-                            
+                            {this.state.info["Recommendee"].map((item, i) =>  (
+								<tr>
+									{this.columns.map((colitem) => (
+										<td>{item[colitem]}</td>
+									))}
+								</tr>
+							))}
                         </tbody>
                     </table>
-                </Form.Group>
+                </Form.Group> */}
+				<div className="table-custom">
+					{/* https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/about.html  */}
+					<BootstrapTable keyField="id" data={this.state.info["Recommendee"]} columns={this.columns}/>
+				</div>
                 <Modal.Footer>
                     <Button type="submit">Save Changes</Button>
                 </Modal.Footer>
@@ -133,7 +163,7 @@ export default function CustomerDescription(props) {
             </Modal.Body>
         </Modal.Dialog>
         </div>
-    )
+    )}
 }
 
 function InputGroupCustom(props) {
